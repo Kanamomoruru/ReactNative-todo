d8e81660c177057142d8546d605193f2a8e4f6c1
Kanata Shimomoto <thunderpooh3@gmail.com>
2020-04-01 13:36:06 +0900

    first init

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Colors from './Colors'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo <Text style={{fontWeight: "300", color: Colors.blue}}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  divider: {
+    backgroundColor: Colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: Colors.black,
+    paddingHorizontal: 64
+  }
+});
